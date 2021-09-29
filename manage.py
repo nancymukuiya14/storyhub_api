928c0ac44a9a3e036bb8a5f45bde01382d834569
nancy.kigotho@student.moringaschool.com <nancykigotho14@gmail.com>
2021-09-29 17:14:24 +0300

    deploy to heroku

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from app.models import User,Blogpost,Comment
 
 
 # creating app instance
-app = create_app('development')
+app = create_app('production')
 
 migrate = Migrate(app,db)
 manager = Manager(app)
